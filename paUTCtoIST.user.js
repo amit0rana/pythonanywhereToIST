3528250fe79ef9b07b31f7c24b5ffaeecc33c151
Amit Rana <amit.rana@gmail.com>
2025-03-16 08:45:54 +0530

    versioning changes, introudce meta

--- paUTCtoIST.user.js
+++ paUTCtoIST.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         PythonAnywhere UTC to IST Converter
-// @namespace    http://tampermonkey.net/
+// @namespace   https://github.com/amit0rana/pythonanywhereToIST
 // @version      0.1
 // @description  Convert UTC time to IST time on PythonAnywhere
-// @author       User
+// @author       Amit
 // @match        https://www.pythonanywhere.com/*
 // @grant        none
+// @downloadURL  https://github.com/amit0rana/pythonanywhereToIST/raw/refs/heads/main/paUTCtoIST.user.js
+// @updateURL    https://github.com/amit0rana/pythonanywhereToIST/raw/refs/heads/main/paUTCtoIST.meta.js
 // ==/UserScript==
 
 (function() {
